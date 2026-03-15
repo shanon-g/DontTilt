8d26b1169e75e0fb796b71ac461a869173091433
Shanon <shn.tw9@gmail.com>
2026-03-16 00:43:15 +0700

    Initial Commit

---
+++ DontTilt/DontTiltApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontTiltApp.swift
+//  DontTilt
+//
+//  Created by Shanon Giuly Istanto on 16/03/26.
+//
+
+import SwiftUI
+
+@main
+struct DontTiltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
